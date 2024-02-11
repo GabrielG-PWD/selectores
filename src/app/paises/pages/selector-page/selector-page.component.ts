dd20ca6729a073d347ef2f8acc882b5089eb879a
Gabriel <85773507+GabrielG-PWD@users.noreply.github.com>
2024-02-11 18:00:34 -0300

    selector de regiones

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', [Validators.required]]
+  });
 
+  regiones: string[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private ps: PaisesService
+  ) { }
+
+  ngOnInit(): void {
+    this.regiones = this.ps.regiones;
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value)
+  }
 }
